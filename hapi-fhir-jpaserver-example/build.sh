dbb26b4745f1c36a3d7421f1f8b0ccf746b67021
Gijsbert van den Brink <vandenbrink@zorgdomein.nl>
2016-07-11 15:38:18 +0200

    Have test servers use DSTU3.

--- hapi-fhir-jpaserver-example/build.sh
+++ hapi-fhir-jpaserver-example/build.sh
@@ -2,5 +2,5 @@
 
 mvn package && \
   docker build --rm --no-cache -t zorgdomein/hapi-fhir-jpaserver-example . && \
-  docker tag zorgdomein/hapi-fhir-jpaserver-example registry.zorgdomein.nl/zorgdomein/hapi-fhir-jpaserver-example:0.1
+  docker tag zorgdomein/hapi-fhir-jpaserver-example registry.zorgdomein.nl/zorgdomein/hapi-fhir-jpaserver-example:0.2
 
--- hapi-fhir-jpaserver-example/src/main/java/ca/uhn/fhir/jpa/demo/FhirTesterConfig.java
+++ hapi-fhir-jpaserver-example/src/main/java/ca/uhn/fhir/jpa/demo/FhirTesterConfig.java
@@ -41,13 +41,13 @@ public class FhirTesterConfig {
 		retVal
 			.addServer()
 				.withId("home")
-				.withFhirVersion(FhirVersionEnum.DSTU2)
-				.withBaseUrl("${serverBase}/baseDstu2")
-				.withName("Local Tester")
+				.withFhirVersion(FhirVersionEnum.DSTU3)
+				.withBaseUrl("${serverBase}/baseDstu3")
+				.withName("ZorgDomein FHIR Server (STU3)")
 			.addServer()
 				.withId("hapi")
-				.withFhirVersion(FhirVersionEnum.DSTU2)
-				.withBaseUrl("http://fhirtest.uhn.ca/baseDstu2")
+				.withFhirVersion(FhirVersionEnum.DSTU3)
+				.withBaseUrl("http://fhirtest.uhn.ca/baseDstu3")
 				.withName("Public HAPI Test Server");
 		return retVal;
 	}
