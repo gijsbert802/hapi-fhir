d465bcf92669534c3183bb174dd2ae05b3dbc072
Gijsbert van den Brink <vandenbrink@zorgdomein.nl>
2016-07-11 14:51:19 +0200

    Add Dockerfile and run+build scripts

---
+++ hapi-fhir-jpaserver-example/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mvn package && \
+  docker build --rm --no-cache -t zorgdomein/hapi-fhir-jpaserver-example . && \
+  docker tag zorgdomein/hapi-fhir-jpaserver-example registry.zorgdomein.nl/zorgdomein/hapi-fhir-jpaserver-example:0.1
+
---
+++ hapi-fhir-jpaserver-example/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -d --name hapi-fhir-jpaserver-example -p 8580:8080 -v /data/jetty-target:/var/lib/jetty/target registry.zorgdomein.nl/zorgdomein/hapi-fhir-jpaserver-example:0.1
+
