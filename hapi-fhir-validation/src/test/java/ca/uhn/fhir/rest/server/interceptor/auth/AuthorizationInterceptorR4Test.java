3fe1bbc47fd715e1e10152d2bea3dbdc30815950
Gijsbert van den Brink <vandenbrink@zorgdomein.nl>
2024-09-09 22:19:18 +0200

    Revert "Fix typo."
    This reverts commit e660868d1f05fe9ad5b3b985197f3724e985b13f.

--- hapi-fhir-validation/src/test/java/ca/uhn/fhir/rest/server/interceptor/auth/AuthorizationInterceptorR4Test.java
+++ hapi-fhir-validation/src/test/java/ca/uhn/fhir/rest/server/interceptor/auth/AuthorizationInterceptorR4Test.java
@@ -134,7 +134,7 @@ public class AuthorizationInterceptorR4Test extends BaseValidationTestWithInline
 		.registerProvider(new DummyServiceRequestResourceProvider())
 		.registerProvider(new DummyConsentResourceProvider())
 		.registerProvider(new PlainProvider())
-		.registerProvider(new DummyCompositionProvider())
+		.registerProvider(new DummyCompositionProvier())
 		.setDefaultResponseEncoding(EncodingEnum.JSON)
 		.withPagingProvider(new FifoMemoryPagingProvider(10))
 		.setDefaultPrettyPrint(false);
@@ -4766,7 +4766,7 @@ public class AuthorizationInterceptorR4Test extends BaseValidationTestWithInline
 
 	}
 
-	public static class DummyCompositionProvider implements IResourceProvider {
+	public static class DummyCompositionProvier implements IResourceProvider {
 
 		@Override
 		public Class<? extends IBaseResource> getResourceType() {
